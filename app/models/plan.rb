8f6520c026f78ccccd3a2c128530c7819707c689
mattkiernan <matt.kiernan@gmail.com>
2015-03-22 10:56:55 -0400

    connect models

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,5 @@
 class Plan < ActiveRecord::Base
+  has_many :cost_structures
+  has_many :plan_features
+  has_many :features, through: :plan_features
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+  has_many :plans
+  has_many :product_features
+  has_many :features, through: :product_features
 end
